c18e2311063f30df3c6bc94cd80a1ba93cd438c8
Aaron Neslin <aneslin125@gmail.com>
2020-01-10 09:56:34 -0500

    now with comments!

--- project.py
+++ project.py
@@ -13,11 +13,14 @@ session = DBsession()
 
 @app.route('/car/<int:id>', methods = ['GET','PUT'])
 def get_car(id):
+    #get a single record by id
     if request.method == 'GET':
         car = session.query(Car).filter_by(id = id).one()
         return jsonify(Car_result = car.serialize)
     if request.method == 'PUT':
+        #update a single record by id
         car = session.query(Car).filter_by(id=id).one()
+        #an inelegant solution until I figure out something better
         for key, values in request.json.items():
             if key == "model":
                 car.model = values
@@ -29,17 +32,20 @@ def get_car(id):
                 car.license_plate = values
         session.add(car)
         session.commit()
+        #return updated record
         return jsonify(car = car.serialize)
 
 
 
 @app.route('/car/post', methods = ['POST'])
 def post_new():
+    #create new record
     if request.method == 'POST':
         new_car = Car(model = request.json["model"], color = request.json["color"],
                      year = request.json["year"], license_plate = request.json["license_plate"])
         session.add(new_car)
         session.commit()
+        #it would be nice to return the new record, but easier to return all?
         return "201, OK"
 
 
